f346ff181aa07681e7572e5f1fdc8ea4722da9b0
Tracy Fisher <webstudent@PC16278.local>
2015-02-23 21:03:19 -0600

    Wk 6 MyFavMovies
    Great Movies!

--- MyFavMovies/DetailViewController.swift
+++ MyFavMovies/DetailViewController.swift
@@ -10,36 +10,61 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var ProductImageView: UIImageView!
+    //@IBOutlet weak var detailDescriptionLabel: UILabel!
 
 
     var detailItem: AnyObject? {
         didSet {
             // Update the view.
-            self.configureView()
+            //self.configureView()
         }
     }
+    
+    var productName: NSString? {
+        didSet {
+            
+        }
+    }
+    
+    var productURL: NSString? {
+        didSet {
+            // Update the view.
+            // self.configureView()
+        }
+    }
+
 
     func configureView() {
         // Update the user interface for the detail item.
         if let detail: AnyObject = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.description
-            }
+            ProductImageView.image = UIImage(named:productURL!)
+            
         }
+    
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        println("Product URL: \(productURL)")
+        println("Product Name: \(productName)")
+        if productName == nil
+        {
+            ProductImageView.image = UIImage(named:"Main.png")
+        }
+        title = productName
         self.configureView()
     }
+
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
 }
+
 
